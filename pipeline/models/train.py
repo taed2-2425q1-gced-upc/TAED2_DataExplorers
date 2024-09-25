ed48796dd7180e06f91bd7da987ea89eb0b79e68
martinaalba21 <martina.alba@estudiantat.upc.edu>
2024-09-25 12:26:19 +0200

    Add pipeline

---
+++ pipeline/models/train.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob as gb
+import cv2
+import tensorflow as tf
+import pickle
+from pathlib import Path
+import keras
+import mlflow
+import pandas as pd
+import yaml
+from codecarbon import EmissionsTracker
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+from config import METRICS_DIR, MODELS_DIR, PROCESSED_DATA_DIR
+
+mlflow.set_experiment("image-classification")
+mlflow.sklearn.autolog(log_model_signatures=False, log_datasets=False)
+
+with mlflow.start_run():
+    # Path of the parameters file
+    #params_path = Path("params.yaml")
+
+    # Path of the prepared data folder
+    input_folder_path = PROCESSED_DATA_DIR
+
+    # Read training dataset
+    x_train = pd.read_csv(input_folder_path / "x_train.npy")
+    y_train = pd.read_csv(input_folder_path / "y_train.npy")
+
+    '''
+    # Read data preparation parameters
+    with open(params_path, "r", encoding="utf8") as params_file:
+        try:
+            params = yaml.safe_load(params_file)
+            params = params["train"]
+        except yaml.YAMLError as exc:
+            print(exc)
+    '''
+
+    # ============== #
+    # MODEL TRAINING #
+    # ============== #
+
+    # Specify the model
+    KerasModel = keras.models.Sequential([
+        keras.layers.Conv2D(200,kernel_size=(3,3),activation='relu',input_shape=(s,s,3)),
+        keras.layers.Conv2D(150,kernel_size=(3,3),activation='relu'),
+        keras.layers.MaxPool2D(4,4),
+        keras.layers.Conv2D(120,kernel_size=(3,3),activation='relu'),    
+        keras.layers.Conv2D(80,kernel_size=(3,3),activation='relu'),    
+        keras.layers.Conv2D(50,kernel_size=(3,3),activation='relu'),
+        keras.layers.MaxPool2D(4,4),
+        keras.layers.Flatten() ,    
+        keras.layers.Dense(120,activation='relu') ,    
+        keras.layers.Dense(100,activation='relu') ,    
+        keras.layers.Dense(50,activation='relu') ,        
+        keras.layers.Dropout(rate=0.5) ,            
+        keras.layers.Dense(6,activation='softmax') ,    
+        ])
+    
+    KerasModel.compile(optimizer ='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+    # For the sake of reproducibility, set the `random_state`
+    model = KerasModel(random_state=2024)
+
+    # Track the CO2 emissions of training the model
+    emissions_output_folder = METRICS_DIR
+    with EmissionsTracker(
+        project_name="image-classification",
+        measure_power_secs=1,
+        tracking_mode="process",
+        output_dir=emissions_output_folder,
+        output_file="emissions.csv",
+        on_csv_write="append",
+        default_cpu_power=45,
+    ):
+        # Then fit the model to the training data
+        model.fit(x_train, y_train, epochs=40,batch_size=64,verbose=1)
+
+    # Log the CO2 emissions to MLflow
+    emissions = pd.read_csv(emissions_output_folder / "emissions.csv")
+    emissions_metrics = emissions.iloc[-1, 4:13].to_dict()
+    emissions_params = emissions.iloc[-1, 13:].to_dict()
+    mlflow.log_params(emissions_params)
+    mlflow.log_metrics(emissions_metrics)
+
+    # Save the model as a pickle file
+    Path("models").mkdir(exist_ok=True)
+
+    with open(MODELS_DIR / "model.pkl", "wb") as pickle_file:
+        pickle.dump(model, pickle_file)
+
---
+++ pipeline/preprocessing.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob as gb
+import cv2
+import tensorflow as tf
+import keras
+from config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+import warnings
+from pathlib import Path
+warnings.filterwarnings('ignore')
+
+input_folder_path = RAW_DATA_DIR
+
+train_path = Path(input_folder_path / "seg_train")
+test_path = Path(input_folder_path / "seg_test")
+predict_path = Path(input_folder_path / "seg_pred")
+
+code = {'buildings':0 ,'forest':1,'glacier':2,'mountain':3,'sea':4,'street':5}
+
+def getcode(n) : 
+    for x , y in code.items() : 
+        if n == y : 
+            return x   
+        
+size = []
+for folder in  os.listdir(train_path) : 
+    files = gb.glob(pathname= str(train_path / folder / '*.jpg'))
+    for file in files: 
+        image = plt.imread(file)
+        size.append(image.shape)
+pd.Series(size).value_counts()
+
+size = []
+for folder in  os.listdir(test_path) : 
+    files = gb.glob(pathname= str( test_path / folder / '*.jpg'))
+    for file in files: 
+        image = plt.imread(file)
+        size.append(image.shape)
+pd.Series(size).value_counts()
+
+size = []
+files = gb.glob(pathname= str(predict_path  / '*.jpg'))
+for file in files: 
+    image = plt.imread(file)
+    size.append(image.shape)
+pd.Series(size).value_counts()
+
+s = 100
+
+x_train = []
+y_train = []
+for folder in  os.listdir(train_path) : 
+    files = gb.glob(pathname= str( train_path  / folder / '*.jpg'))
+    for file in files: 
+        image = cv2.imread(file)
+        image_array = cv2.resize(image , (s,s))
+        x_train.append(list(image_array))
+        y_train.append(code[folder])
+
+x_test = []
+y_test = []
+for folder in  os.listdir(test_path) : 
+    files = gb.glob(pathname= str(test_path / folder / '*.jpg'))
+    for file in files: 
+        image = cv2.imread(file)
+        image_array = cv2.resize(image , (s,s))
+        x_test.append(list(image_array))
+        y_test.append(code[folder])
+
+x_pred = []
+files = gb.glob(pathname= str(predict_path / '*.jpg'))
+for file in files: 
+    image = cv2.imread(file)
+    image_array = cv2.resize(image , (s,s))
+    x_pred.append(list(image_array))     
+
+x_train = np.array(x_train)
+x_test = np.array(x_test)
+x_pred_array = np.array(x_pred)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+prepared_folder_path = PROCESSED_DATA_DIR
+
+x_train_path = Path(prepared_folder_path / "x_train.npy")
+x_test_path = Path(prepared_folder_path / "x_test.npy")
+x_pred_array_path = Path(prepared_folder_path / "x_pred_array.npy")
+y_train_path = Path(prepared_folder_path / "y_train.npy")
+y_test_path = Path(prepared_folder_path / "y_test.npy")
+
+print('Data correcly processed.')
+
+
+np.save(x_train_path, x_train)
+np.save(x_test_path, x_test)
+np.save(x_pred_array_path, x_pred_array)
+np.save(y_train_path, y_train)
+np.save(y_test_path, y_test)
+
+print('Data correcly saved.')
+
+#loaded_array = np.load(x_train_path)
