577d54f4aae88b38504ee4a8cd554cdcf1f73d48
Martina Albà González <106911781+martinaalba21@users.noreply.github.com>
2024-10-23 18:24:02 +0200

    Update evaluate test

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,16 +1,19 @@
+"""
+Tests for evaluate functions
+"""
 from pathlib import Path
+from unittest.mock import patch, MagicMock, mock_open
 import pytest
 import numpy as np
-from unittest.mock import patch, MagicMock, mock_open
-import json
-from src.models import evaluate 
+from src.models import evaluate
 from src.config import PROCESSED_DATA_DIR, MODELS_DIR
 
+# pylint: disable=C0301
 
 @pytest.fixture
 def validation_data():
     """Fixture to provide mock validation data."""
-    x_valid = np.random.rand(50, 10)  
+    x_valid = np.random.rand(50, 10)
     y_valid = np.random.randint(0, 2, size=(50, 1))
     return x_valid, y_valid
 
@@ -20,8 +23,8 @@ def test_load_validation_data(mock_npy_load):
     """Test to ensure validation data is loaded correctly."""
     # Mock the np.load function to return dummy data
     mock_npy_load.side_effect = [
-        np.random.rand(50, 10), 
-        np.random.randint(0, 2, size=(50, 1))  
+        np.random.rand(50, 10),
+        np.random.randint(0, 2, size=(50, 1))
     ]
 
     x_valid, y_valid = evaluate.load_validation_data(PROCESSED_DATA_DIR)
@@ -54,13 +57,14 @@ def test_evaluate_model(mock_load_model, validation_data):
 
 @pytest.fixture
 def mock_paths():
+    """Mock directory paths for testing."""
     with patch("src.models.evaluate.PROCESSED_DATA_DIR", Path("/fake_path")) as processed_mock:
         with patch("src.models.evaluate.MODELS_DIR", Path("/fake_model_dir")) as models_mock:
             with patch("src.models.evaluate.METRICS_DIR", Path("/fake_metrics_dir")) as metrics_mock:
                 yield processed_mock, models_mock, metrics_mock
 
 def test_main(mock_paths):
-    # Arrange
+    """Test the main evaluation process."""
     mock_x = [np.array(np.zeros((100, 100, 3), dtype=np.uint8)), np.array(np.zeros((100, 100, 3), dtype=np.uint8)), np.array(np.zeros((100, 100, 3), dtype=np.uint8))]
     mock_y = [1, 2, 3]
     mock_accuracy = 0.95
